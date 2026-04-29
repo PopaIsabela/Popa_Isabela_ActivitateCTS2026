2050408b845dd248d997a2e2455a7d0bf6bc6946
Isabella Popa <popaisabela22@stud.ase.ro>
2026-04-29 14:04:49 +0300

    Medicament (A.5)

---
+++ Seminar01/src/main/java/cts/spital/model/MedicamentSpital.java
@@ -0,0 +1,11 @@
+public class MedicamentSpital {
+
+    public void achizitioneazaMedicament() {
+        prezintaReteta();
+        System.out.println("Medicament cumpărat din spital");
+    }
+
+    public void prezintaReteta() {
+        System.out.println("Se verifică rețeta...");
+    }
+}
